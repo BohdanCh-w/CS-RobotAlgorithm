5d18e2e671d61ef8f87b1acfd9f80f4a248d9e11
BohdanCh-w <bohdanch.w@gmail.com>
2021-09-21 12:59:53 +0300

    Round counter. Preffered changed

--- ChepeliukBohdan.RobotChallange/ChepeliukBohdanAlgorithm.cs
+++ ChepeliukBohdan.RobotChallange/ChepeliukBohdanAlgorithm.cs
@@ -3,38 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Robot.Common;
-using Robot.Tournament;
 
 namespace ChepeliukBohdan.RobotChallange {
 
     public class ChepeliukBohdanAlgorithm : IRobotAlgorithm {
-        public static HashSet<Position> prefferedPositions = new HashSet<Position>();
-        private static int roundNumber = 0;
+        public static Dictionary<int, Position> prefferedPositions = new Dictionary<int, Position>();
+        public static int roundNumber = 0;
         private const int maxRoundNumber = 50;
-        //private void Logger_OnLogRound(object sender, LogRoundEventArgs e) => ++this.RoundCount;
+
+        public ChepeliukBohdanAlgorithm() => Logger.OnLogRound += new LogRoundEventHandler(this.Logger_OnLogRound);
+
+        private void Logger_OnLogRound(object sender, LogRoundEventArgs e) => ++roundNumber;
+
+        public void incrementRound(object sender, Robot.Tournament.LogRoundEventArgs e) {
+            roundNumber++;
+        }
 
         public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map) {
             int robotCount = robots.Where(n => n.OwnerName == Author).Count();
             Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
-            prefferedPositions.Remove(movingRobot.Position);
 
-            if ((movingRobot.Energy > 600) && (robots.Count < map.Stations.Count) && roundNumber < maxRoundNumber - 5) {
+            EnergyStation nearestStation = HelperMethods.FindNearestFreeStation(movingRobot, map, robots);
+
+            if ((movingRobot.Energy > 1000) && (robots.Count < map.Stations.Count) && roundNumber < maxRoundNumber - 10 &&
+                HelperMethods.FindDistance(nearestStation.Position, movingRobot.Position) < 400) {
                 return new CreateNewRobotCommand();
             }
 
-            EnergyStation nearestStation = HelperMethods.FindNearestFreeStation(robots[robotToMoveIndex], map, robots);
-            if (nearestStation.Position == movingRobot.Position && nearestStation.Energy > 0)
+            int nearestStationEnergy = HelperMethods.StationEnergy(movingRobot.Position, map);
+            if (nearestStationEnergy != -1) // && (nearestStationEnergy > 0 || movingRobot.Energy < 500)
                 return new CollectEnergyCommand();
 
             Position move = NextTarget(movingRobot, map, robots);
-            prefferedPositions.Add(move);
             move = HelperMethods.OptimalMove(movingRobot.Position, move, movingRobot.Energy);
+            prefferedPositions[robotToMoveIndex] = move;
 
             if (move == movingRobot.Position) {
+                prefferedPositions[robotToMoveIndex] = null;
                 move = HelperMethods.MinimalMove(movingRobot.Position, nearestStation.Position, movingRobot.Energy);
-                prefferedPositions.Remove(move);
             }
-
 
             return new MoveCommand() { NewPosition = move };
         }
@@ -42,16 +49,17 @@ namespace ChepeliukBohdan.RobotChallange {
         public Position NextTarget(Robot.Common.Robot movingRobot, Map map, IList<Robot.Common.Robot> robots) {
             const double stationCoef = 1000.0;
             const double proximityCoef = 100.0;
-            const double attackCoef = 10.0;
+            const double attackCoef = 0.01;
             var targets = new Dictionary<Position, double> { };
 
             foreach (var station in map.Stations) {
-                if (!prefferedPositions.Contains(station.Position)
+                if (!prefferedPositions.Values.Contains(station.Position)
                     && HelperMethods.IsStationFree(station, movingRobot, robots)) {
                     int dist = HelperMethods.FindDistance(station.Position, movingRobot.Position);
                     targets[station.Position] = stationCoef / dist * station.Energy;
                     if (targets.Count != 0) {
-                        foreach (var otherPos in targets.Keys) {
+                        var keys = new List<Position>(targets.Keys);
+                        foreach (var otherPos in keys) {
                             if (otherPos != station.Position) {
                                 double proximity = proximityCoef / HelperMethods.FindDistance(station.Position, otherPos);
                                 targets[station.Position] += proximity;
@@ -62,14 +70,12 @@ namespace ChepeliukBohdan.RobotChallange {
                 }
             }
             foreach (var robot in robots.Where(n => n.OwnerName != Author).ToList()) {
-                int profit = 0;
-                if ((profit = HelperMethods.AttackRevenue(movingRobot, robot)) > 0) {
-                    double prior = profit * attackCoef / HelperMethods.FindDistance(movingRobot.Position, robot.Position);
-                    if (targets.ContainsKey(robot.Position))
-                        targets[robot.Position] += prior;
-                    else
-                        targets[robot.Position] = prior;
-                }
+                int profit = HelperMethods.AttackRevenue(movingRobot, robot);
+                double prior = profit * attackCoef / HelperMethods.FindDistance(movingRobot.Position, robot.Position) * movingRobot.Energy;
+                if (targets.ContainsKey(robot.Position))
+                    targets[robot.Position] += prior;
+                else
+                    targets[robot.Position] = prior;
             }
             return targets.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
         }
